103514f90f9cd84d560ffc70863f7b2bdeaee452
DAEDR17 <DAEDR17@AAXA22A520.ad.cbp.dhs.gov>
2018-11-27 08:59:45 -0500

    Initial cehck in of sales tool

---
+++ SalesTool/src/com/test/Main.java
@@ -0,0 +1,18 @@
+package com.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		displayGreeting();
+		data.display();
+
+	}
+	
+	private static void displayGreeting()
+	{
+		System.out.println("Hello There");
+		System.out.println("This app shows sales data");
+	}
+
+}
